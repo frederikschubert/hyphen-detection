1108f4f9b20754222468c40f2a2a2a8ac2b90d74
Frederik Schubert <frederik.schubert@mailbox.org>
2021-01-06 08:36:01 +0100

    improve evaluation code

--- eval.py
+++ eval.py
@@ -1,8 +1,11 @@
+from collections import OrderedDict
+import csv
 from PIL import Image
 import torch
 import math
 from common.hyphen_dataset import read_patch
 import os
+import torch.nn.functional as F
 
 import numpy as np
 from common.utils import get_detection_image, visualize_predictions
@@ -40,9 +43,8 @@ class EvalArguments(Arguments):
     eval_dir: str = "/home/schubert/projects/hyphen/data/Chile_Input_2/LCN/Biotit"
     artifact_name: str = "patch_detector"
     artifact_tag: str = "latest"
-
-    def process_args(self):
-        self.debug = True
+    threshold: float = 0.99
+    num_samples: int = np.infty
 
 
 def main():
@@ -52,11 +54,12 @@ def main():
         tags=args.tags,
         job_type="eval",
         config=args.as_dict(),
-        mode="dryrun" if args.debug else "run",
+        mode="disabled" if args.debug else "run",
     )
     logger.info("Restoring model...")
     model: HyphenDetection = HyphenDetection.load_from_checkpoint(
-        str(get_artifact_path(args.artifact_name, args.artifact_tag) / "best.ckpt")
+        str(get_artifact_path(args.artifact_name, args.artifact_tag) / "best.ckpt"),
+        dataset=args.dataset,
     )
     model.to("cuda")
     model.eval()
@@ -65,26 +68,64 @@ def main():
     centers = model.val_dataset.get_centers_for_image(model.val_dataset.image_paths[0])
     logger.debug("Centers: {}", len(centers))
     with torch.no_grad():
-        for i, image_path in tqdm(enumerate(images), total=len(images)):
-            logger.debug(image_path)
-            image = Image.open(image_path).convert("RGB")
-            image = np.array(image)
-            predictions = []
-            for center in tqdm(centers):
-                patch = read_patch(image, center, args.patch_size).to(model.device)
-                prediction = model(patch).argmax().item()
-                predictions.append(prediction)
-            logger.info("Percentage: {}", sum(predictions) / len(predictions))
-            if args.debug:
-                detection_image = get_detection_image(image, centers, predictions)
-                wandb.log(
+        with open(
+            os.path.join(wandb.run.dir, "percentages.csv"), "w", newline=""
+        ) as csv_file:
+            fieldnames = [
+                "mineral",
+                "sample",
+                "points",
+                "points_on_hyphae",
+                "percent",
+            ]
+            writer = csv.DictWriter(
+                csv_file,
+                fieldnames=fieldnames,
+            )
+            writer.writeheader()
+            percentages = []
+            rows = []
+            for i, image_path in tqdm(enumerate(sorted(images)), total=len(images)):
+                logger.trace(image_path)
+                image = Image.open(image_path).convert("RGB")
+                image = np.array(image)
+                predictions = []
+                for center in tqdm(centers):
+                    patch = read_patch(image, center, args.patch_size).to(model.device)
+                    prediction = F.softmax(model(patch).squeeze(), dim=-1)
+                    predictions.append(1 if prediction[1] > args.threshold else 0)
+                percentage = round(sum(predictions) / len(predictions) * 100, 2)
+                percentages.append(percentage)
+                row = OrderedDict(
                     {
-                        f"sample_{i}": wandb.Image(
-                            detection_image, caption=str(image_path)
-                        )
-                    },
+                        "mineral": os.path.basename(args.eval_dir),
+                        "sample": os.path.basename(image_path)
+                        .split(".")[0]
+                        .split("_Laser")[0],
+                        "points": len(centers),
+                        "points_on_hyphae": sum(predictions),
+                        "percent": percentage,
+                    }
                 )
-                break
+                # TODO(frederik): create svg file with markers
+                # TODO(frederik): generalize this to multiple minerals
+                writer.writerow(row)
+                rows.append(list(row.values()))
+                csv_file.flush()
+                if i > args.num_samples:
+                    break
+            mean_percentage = round(np.mean(percentages), 2)
+            logger.info(
+                "Mean Percentage for {} is {}",
+                args.eval_dir,
+                mean_percentage,
+            )
+            wandb.log(
+                {
+                    "mean_percentage": mean_percentage,
+                    "results": wandb.Table(columns=fieldnames, data=rows),
+                }
+            )
 
 
 if __name__ == "__main__":
